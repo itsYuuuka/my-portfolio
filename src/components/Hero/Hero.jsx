b596fddfee1755790ca8dd709e25797013ce2b56
itsYuuuka <okan_altun@hotmail.com>
2025-03-18 16:00:20 +0100

    Add Hero component with profile image, social icons, and resume download
    - Imported and displayed the profile image
    - Added mdi for LinkedIn, GitHub, Spotify, and a sun icon for theme mode
    - Integrated a resume download button linking to a PDF

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,60 @@
+import styles from "./HeroStyles.module.css";
+import profileImg from "../../assets/profile-picture.png";
+import Icon from "@mdi/react";
+import { mdiWhiteBalanceSunny } from "@mdi/js";
+import { mdiLinkedin } from "@mdi/js";
+import { mdiGithub } from "@mdi/js";
+import { mdiSpotify } from "@mdi/js";
+import CV from "../../assets/Okan-Altun-cv.pdf";
+
+function Hero() {
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={profileImg}
+          alt="Profile Picture of Okan Altun"
+        />
+        <Icon
+          path={mdiWhiteBalanceSunny}
+          size={1}
+          className={styles.colorMode}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Okan
+          <br />
+          Altun
+        </h1>
+        <h2>Junior Software Developer</h2>
+        <span>
+          <a
+            href="https://www.linkedin.com/in/okan-altun-478a30336/"
+            target="_blank"
+          >
+            <Icon path={mdiLinkedin} size={1} color="#000"/>
+          </a>
+          <a href="https://github.com/itsYuuuka" target="_blank">
+            <Icon path={mdiGithub} size={1} color="#000"/>
+          </a>
+          <a
+            href="https://open.spotify.com/user/wm9hzk1b3o0vt02yjmk683jrj?si=779e88c2fdd84ebf"
+            target="_blank"
+          >
+            <Icon path={mdiSpotify} size={1} color="#000"/>
+          </a>
+        </span>
+        <p>With a passion for developing modern web apps.</p>
+        <a href={CV} download>
+          <button className="hover">
+            My Resume
+          </button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
