01f4a4863594528640fe1431827ef93a78cc84ca
itsYuuuka <okan_altun@hotmail.com>
2025-04-23 00:28:45 +0200

    Add iMock project card to Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import styles from "./ProjectsStyles.module.css";
 import sessionConnect from "../../assets/SCLogo_blue.svg";
 import cinemaProject from "../../assets/cinema-project.svg";
 import badUiChallenge from "../../assets/bad-ui-challenge.svg";
+import iMock from "../../assets/i-mock.svg";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -27,6 +28,12 @@ function Projects() {
           link={"https://github.com/web-altun/bad-ui-challenge"}
           h3="Bad Ui Challenge"
           p="The Bad UI Challenge is a project where I intentionally built a website using HTML, CSS, and JavaScript to deliver a purposely awful user experience."
+        />
+        <ProjectCard
+          src={iMock}
+          link={"https://github.com/itsYuuuka/i-mock"}
+          h3="iMock"
+          p="Recreated from the ground up, iMock mirrors the iconic iPhone site. It’s not innovation — it’s imitation at its finest."
         />
       </div>
     </section>
